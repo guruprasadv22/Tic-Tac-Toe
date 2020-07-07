3134bfa1f304e68a1e3ee14f11e6b84ab878b6cb
guruprasadv22 <guruprasadv22@gmail.com>
2020-07-07 22:56:50 +0530

    Hope I get this working...

--- program.py
+++ program.py
@@ -1,3 +1,17 @@
-Board = [x for x in range(10)]
+
 def insertBoard(letter,pos):
     Board[pos] = letter
+
+def  isFree(pos):
+    if board[pos] == ' ':
+        return True
+
+def isWinner(board, letter):
+    return((board[1] and board[2] and board[3]) or
+    (board[4] and board[5] and board[6]) or
+    (board[7] and board[8] and board[9]) or
+    (board[1] and board[4] and board[7]) or
+    (board[2] and board[5] and board[8]) or
+    (board[3] and board[6] and board[9]) or
+    (board[1] and board[5] and board[9]) or
+    (board[3] and board[5] and board[7]))
